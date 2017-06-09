7d21c35ab261ade4e5e2b114b9f45757d9a5cb86
David <david_elvir@unitec.edu>
2017-06-09 14:34:17 -0600

    prueba

--- Racional.h
+++ Racional.h
@@ -19,7 +19,58 @@ class Racional : public Real{
     int getDenominador();
     void setDenominador(int);
 
+    virtual string operator+ (Racional& racional){
+        Racional* resultado = new Racional();
+        string res="";
+        if(denominador == racional.getDenominador()){
+            resultado->setNumerador(numerador+racional.getNumerador());
+            resultado->setDenominador(denominador);
+        }else{
+            int A1;
+            int A2;
+            int B;
 
+            A1 = numerador * racional.getDenominador();
+            A2 = racional.getNumerador() * denominador;
+
+            B = racional.getDenominador() * denominador;
+
+            resultado->setNumerador(A1+A2);
+            resultado->setDenominador(B);
+        }
+
+        int resN = resultado->getNumerador();
+        int resD = resultado->getDenominador();
+
+        //char *tem1 = itoa(resN);
+        //char *tem2 = itoa(resD);
+
+        string resN1 = to_string(resN);
+        string resD1 = to_string(resD);
+        
+        res+= resN1 + "/" + resD1;
+        return res;
+
+    }
+    /*virtual string operator+ (Radical& radical) = 0;
+    virtual string operator+ (int) = 0;
+    virtual string operator+ (double) = 0;
+
+    virtual string operator- (Racional& racional) = 0;
+    virtual string operator- (Radical& radical) = 0;
+    virtual string operator- (int) = 0;
+    virtual string operator- (double) = 0;
+
+    virtual string operator* (Racional& racional) = 0;
+    virtual string operator* (Radical& radical) = 0;
+    virtual string operator* (int) = 0;
+    virtual string operator* (double) = 0;
+
+    virtual string operator/ (Racional& racional) = 0;
+    virtual string operator/ (Radical& radical) = 0;
+    virtual string operator/ (int) = 0;
+    virtual string operator/ (double) = 0;*/
+    
 };
 
 #endif
