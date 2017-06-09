531a315a6d001b87a730e88c5544db0a70603adb
Jahazielmr <Jahazielmr97@gmail.com>
2017-06-09 13:27:10 -0600

    Main

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include "Real.h"
+#include "Radical.h"
+#include "Racional.h"
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+
+	cout <<"Hola"<<endl;
+
+
+return 0;	
+}
